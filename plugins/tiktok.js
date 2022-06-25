9306c8dfa6830ed252bd9dc592fb3b01caf12966
Yxbxrock <102041995+Yxbxrock@users.noreply.github.com>
2022-06-25 23:58:53 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -5,8 +5,8 @@ let p = await  hxz.ttdownloader(text)
 const { nowm, wm, audio } = p
 conn.sendFile(m.chat, nowm, null, 'Nih', m)
 }
-handler.help = ['tiktok'].map(v => v + ' <url>')
+handler.help = ['tiktod'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(tiktok)$/i
+handler.command = /^(tiktok|tt|tiktod|tiktokdl)$/i
 
 module.exports = handler
